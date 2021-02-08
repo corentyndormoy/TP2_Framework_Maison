57b9df915bf80e9ba586edbe631226405491a974
corentyndormoy <corentyn.dormoy@gmail.com>
2021-02-08 11:23:17 +0100

    Name form

--- templates/home/index.php
+++ templates/home/index.php
@@ -1,3 +1,8 @@
 <h1>
     Hello <?php echo $name; ?>
 </h1>
+
+<form method="post" action=".">
+    <input type="text" name="name" id="name" class="border" required>
+    <input type="submit" id="btnName" value="Envoyer">
+</form>
